d0c6a2a497eb9ab17ebc613c420d696df436db5b
Khoi Nguyen <minhkhoi632000@gmail.com>
2019-03-29 19:51:56 -0700

    Add files changes during Mesa afternoon 3/29/2019

---
+++ Comp Sci/Assignment_3_Build/Assignment 3 - The Greatest, Least, and Average of These.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	/*Pseudo Code
+	Input:
+		A series of integers
+		( -9999 to end the series) -- a loop
+		initial largest = -9999
+		initial smallest = -9999
+		initial average = 0.0
+	Processing:
+		largest (loop)
+			|
+			input > largest or largest = -9999
+				\
+				largest = input
+
+			
+		smallest (loop)
+			|
+			input < smallest or smallest = -9999
+				\
+				smallest = input
+
+	Output:
+		display the largest
+		the smallest
+		average of all number enter
+	*/
+	int input;
+
+	cout << "Enter a number or -9999 to exit: ";
+
+
+	return 0;
+}
