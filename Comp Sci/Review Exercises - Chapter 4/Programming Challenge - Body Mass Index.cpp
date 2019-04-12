df409280ae7a65cc086e6eeb8fdfab71e66720ea
CyberExplosion <minhkhoi632000@gmail.com>
2019-04-11 21:16:33 -0700

    Nothing new. Same night as last commit

--- Comp Sci/Review Exercises - Chapter 4/Programming Challenge - Body Mass Index.cpp
+++ Comp Sci/Review Exercises - Chapter 4/Programming Challenge - Body Mass Index.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main() {
+int main_done17c() {
+	double weight,
+		height,
+		BMI;
 
+	cout << "Enter your weight then height: ";
+	cin >> weight >> height;
 
+	BMI = weight * (703 / pow(height, 2));
+
+	if (BMI > 18.5&&BMI < 25) {
+		cout << "You have optimal weight\n";
+	}
+	else if (BMI < 18.5) {
+		cout << "You are underweight\n";
+	}
+	else if (BMI > 25) {
+		cout << "You are overweight.\n";
+	}
 
 	return 0;
 }
---
+++ Comp Sci/Review Exercises - Chapter 4/Programming Challenge - Fat Gram Calculator.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main_done17c() {
+	double calories_from_fat,
+		fat_gram,
+		calories,
+		total_calories,
+		percentage;
+
+	cout << "Enter the number of fat in food (in gram): ";
+	cin >> fat_gram;
+	cout << "Enter the number of calories in food: ";
+	cin >> calories;
+
+	if (calories > 0) {
+		if (fat_gram > 0) {
+			calories_from_fat = fat_gram * 9.0;
+			total_calories = calories + calories_from_fat;
+
+			if (calories_from_fat < total_calories) {
+				percentage = calories_from_fat / total_calories;
+
+				if (percentage < 0.3 && percentage > 0) {
+					cout << "The food is low in fat\n";
+					cout << "Fat percentage comparing to food is: " << percentage << endl;
+				}
+				else {
+					cout << "Fat percentage in food is: " << percentage << endl;
+				}
+			}
+			else cout << "The number of calories from fat cannot be more than the total number of calories. Please check your input\n";
+		}
+		else cout << "The fat cannot be 0 or smaller\n";
+	}
+	else cout << "The calories cannot be 0 or smaller\n";
+	
+	return 0;
+}
