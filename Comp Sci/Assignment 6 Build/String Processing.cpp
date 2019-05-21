3a04ba8073a8a921100c4a6d5e4d22b3f8432655
Khoi Nguyen <minhkhoi632000@gmail.com>
2019-05-20 17:24:56 -0700

    Add some files for Assignment 6

---
+++ Comp Sci/Assignment 6 Build/String Processing.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+	char c = 't';
+	if (isupper(c)) {
+		cout << "k";
+	}
+	else cout << "nay";
+/*Pseudo Code
+Input:
+	
+Processing:
+
+Output:
+
+*/
+	return 0;
+}
