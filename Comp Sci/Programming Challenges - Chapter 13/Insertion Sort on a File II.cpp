ff2283ed2b37f6b91d5e25529054b6f5202d10cb
Khoi Nguyen <minhkhoi632000@gmail.com>
2019-07-18 09:48:40 -0700

    Start challenge 12

---
+++ Comp Sci/Programming Challenges - Chapter 13/Insertion Sort on a File II.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+
+int main() {
+	fstream file("insert sort.txt", ios::out | ios::in | ios::binary);
+	//Record of People
+	struct 
+
+	if (file.fail()) {
+		cout << "The file failed to open\n";
+		return 1;
+	}
+
+
+
+	return 0;
+}
