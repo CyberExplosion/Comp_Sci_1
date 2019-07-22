e674167ef55e648f42e351fb453fd967cf3ccae8
CyberExplosion <minhkhoi632000@gmail.com>
2019-07-21 22:21:22 -0700

    Fuck this chapter

--- Comp Sci/Programming Challenges - Chapter 13/Corporate Sales Data Input.cpp
+++ Comp Sci/Programming Challenges - Chapter 13/Corporate Sales Data Input.cpp
@@ -1,107 +1,107 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-using namespace std;
-
-int main() {
-	//enum Division {
-	//	E,
-	//	W,
-	//	N,
-	//	S
-	//};
-
-	double qrt_total[4] = { 0,0,0,0 },
-		division_total[4] = { 0,0,0,0 },
-		total_corp = 0,
-		qrt_avg_division[4] = { 0,0,0,0 },
-		highest_qrt = -9999,
-		lowest_qrt = -9999;
-	char ch;
-	double number;
-	ifstream file;
-	file.open("Corporate Output.txt");
-
-	if (file.fail()) {
-		cout << "File failed successfully\n";
-		return 1;
-	}
-
-	//Total corporate sales for each quarter
-	int div_count = 0;
-	while (!file.eof()) {
-		ch = file.peek();
-		if (isdigit(ch)) {
-			for (int i = 0; i < 4; i++) {
-				file >> number;
-				qrt_total[i] += number;
-			}
-		}
-		else {
-			//Just read and throw away
-			ch = file.get();
-		}
-	}
-	//Display
-	cout << "Total corporate sales for each quarter: \n";
-	for (div_count = 0; div_count < 4; div_count++) {
-		cout << "Quarter " << div_count + 1 << ": " << qrt_total[div_count] << endl;
-	}
-	
-	//Clear the eof state and set it back
-	file.clear();
-	file.seekg(0, file.beg);
-
-	//Total yearly sales for each division
-	div_count = 0;
-	while (!file.eof()) {
-		ch = file.peek();
-		if (isdigit(ch)) {
-			for (int i = 0; i < 4; i++) {
-				file >> number;
-				division_total[div_count] += number;
-			}
-			div_count++;
-		}
-		//Throw away and move on
-		file.get(ch);
-	}
-	//Display
-	cout << "Total yearly sales for each division:\n";
-	for (div_count = 0; div_count < 4; div_count++) {
-		cout << "Division " << div_count + 1 << ": " << division_total[div_count] << endl;
-	}
-
-	//Total yearly corporate sales
-	for (div_count = 0; div_count < 4; div_count++) {
-		total_corp += division_total[div_count];
-	}
-	cout << "The total yearly corporate sales are: " << total_corp << endl;
-
-	//Average quarterly sales for the division
-	for (div_count = 0; div_count < 4; div_count++) {
-		qrt_avg_division[div_count] = division_total[div_count] / 4;
-	}
-	for (div_count = 0; div_count < 4; div_count++) {
-		cout << "Average sales of division on quarter " << div_count + 1 << ": " << qrt_avg_division[div_count] << endl;
-	}
-
-	//The highest and lowest quarters for the corporation
-	int highest, lowest;
-	for (div_count = 0; div_count < 4; div_count++) {
-		if (qrt_total[div_count] > highest_qrt || highest_qrt == -9999) {
-			highest_qrt = qrt_total[div_count];
-			highest = div_count;
-		}
-		if (qrt_total[div_count < lowest_qrt] || lowest_qrt == -9999) {
-			lowest_qrt = qrt_total[div_count];
-			lowest = div_count;
-		}
-	}
-
-	cout << "The highest quarter sales is quarter number " << highest <<". With sales of total: " << highest_qrt << endl;
-	cout << "The lowest quarter sales is quarter number " << lowest << ". With sales of total: " << lowest_qrt << endl;
-
-	file.close();
-	return 0;
-}
+//#include <iostream>
+//#include <fstream>
+//#include <string>
+//using namespace std;
+//
+//int main() {
+//	//enum Division {
+//	//	E,
+//	//	W,
+//	//	N,
+//	//	S
+//	//};
+//
+//	double qrt_total[4] = { 0,0,0,0 },
+//		division_total[4] = { 0,0,0,0 },
+//		total_corp = 0,
+//		qrt_avg_division[4] = { 0,0,0,0 },
+//		highest_qrt = -9999,
+//		lowest_qrt = -9999;
+//	char ch;
+//	double number;
+//	ifstream file;
+//	file.open("Corporate Output.txt");
+//
+//	if (file.fail()) {
+//		cout << "File failed successfully\n";
+//		return 1;
+//	}
+//
+//	//Total corporate sales for each quarter
+//	int div_count = 0;
+//	while (!file.eof()) {
+//		ch = file.peek();
+//		if (isdigit(ch)) {
+//			for (int i = 0; i < 4; i++) {
+//				file >> number;
+//				qrt_total[i] += number;
+//			}
+//		}
+//		else {
+//			//Just read and throw away
+//			ch = file.get();
+//		}
+//	}
+//	//Display
+//	cout << "Total corporate sales for each quarter: \n";
+//	for (div_count = 0; div_count < 4; div_count++) {
+//		cout << "Quarter " << div_count + 1 << ": " << qrt_total[div_count] << endl;
+//	}
+//	
+//	//Clear the eof state and set it back
+//	file.clear();
+//	file.seekg(0, file.beg);
+//
+//	//Total yearly sales for each division
+//	div_count = 0;
+//	while (!file.eof()) {
+//		ch = file.peek();
+//		if (isdigit(ch)) {
+//			for (int i = 0; i < 4; i++) {
+//				file >> number;
+//				division_total[div_count] += number;
+//			}
+//			div_count++;
+//		}
+//		//Throw away and move on
+//		file.get(ch);
+//	}
+//	//Display
+//	cout << "Total yearly sales for each division:\n";
+//	for (div_count = 0; div_count < 4; div_count++) {
+//		cout << "Division " << div_count + 1 << ": " << division_total[div_count] << endl;
+//	}
+//
+//	//Total yearly corporate sales
+//	for (div_count = 0; div_count < 4; div_count++) {
+//		total_corp += division_total[div_count];
+//	}
+//	cout << "The total yearly corporate sales are: " << total_corp << endl;
+//
+//	//Average quarterly sales for the division
+//	for (div_count = 0; div_count < 4; div_count++) {
+//		qrt_avg_division[div_count] = division_total[div_count] / 4;
+//	}
+//	for (div_count = 0; div_count < 4; div_count++) {
+//		cout << "Average sales of division on quarter " << div_count + 1 << ": " << qrt_avg_division[div_count] << endl;
+//	}
+//
+//	//The highest and lowest quarters for the corporation
+//	int highest, lowest;
+//	for (div_count = 0; div_count < 4; div_count++) {
+//		if (qrt_total[div_count] > highest_qrt || highest_qrt == -9999) {
+//			highest_qrt = qrt_total[div_count];
+//			highest = div_count;
+//		}
+//		if (qrt_total[div_count < lowest_qrt] || lowest_qrt == -9999) {
+//			lowest_qrt = qrt_total[div_count];
+//			lowest = div_count;
+//		}
+//	}
+//
+//	cout << "The highest quarter sales is quarter number " << highest <<". With sales of total: " << highest_qrt << endl;
+//	cout << "The lowest quarter sales is quarter number " << lowest << ". With sales of total: " << lowest_qrt << endl;
+//
+//	file.close();
+//	return 0;
+//}
---
+++ Comp Sci/Programming Challenges - Chapter 13/Inventory Program.cpp
@@ -0,0 +1,162 @@
+//Fuck Binary Files
+
+//#include <iostream>
+//#include <fstream>
+//#include <string>
+//#include <vector>
+//#include <iomanip>
+//using namespace std;
+//
+//int main() {
+//	struct Item {
+//		//If you use binary data, string is the enemy, use vector of characters when you think you will have to deal with binary data
+//		vector<char> description;
+//		int quantity;
+//		double whole_cost;
+//		double retail_cost;
+//		vector<char> date;
+//	};
+//	fstream file("inventory.txt", ios::in | ios::out | ios::app | ios::binary);
+//	int choice;
+//	vector<Item>buffer;
+//	int item_index = 0;
+//
+//	if (file.fail()) {
+//		cout << "File opened failed successfully\n";
+//		return 1;
+//	}
+//
+//	Item add,
+//		display,
+//		change;
+//	string description,
+//		date;
+//
+//	cout << "1) Add new records to the file.\n"
+//		<< "2) Display any records in the file.\n"
+//		<< "3) Change any record in the file.\n" << endl;
+//	cout << "Enter what you want to do: ";
+//	cin >> choice;
+//	cin.ignore(100, '\n'); cin.clear(); cin.sync();
+//	switch (choice) {
+//	case 1:
+//		cout << "Enter new item description: ";
+//		getline(cin, description);
+//		//Insert format: .insert(vector position iterator, another vector or string or whatever iterator for begin, another iterator for the end)
+//		add.description.insert(add.description.begin(),description.begin(), description.end());
+//		cout << "Enter the quantity of item: ";
+//		cin >> add.quantity;
+//		cout << "Enter the wholesale cost: ";
+//		cin >> add.whole_cost;
+//		cout << "Enter the retail cost: ";
+//		cin >> add.retail_cost;
+//		cout << "Enter the date add to inventory: ";
+//		cin.ignore(100, '\n'); cin.clear(); cin.sync();
+//		getline(cin, date);
+//		add.date.insert(add.date.begin(),date.begin(), date.end());
+//		//Writing structure meaning that only the instance of structure is counted
+//		//Write to vector
+//		buffer.push_back(add);
+//		//Write structure as binary data to file
+//		file.write((char*)(&buffer), sizeof(buffer));
+//		file.close();
+//		break;
+//	case 2:
+//		 item_index = 0;
+//		cout << "Displaying: \n";
+//		//Read all the data in the file into the vector
+//		while (!file.fail()) {
+//			//Have to insert the thing to work
+//			file.read((&display.description[0]), sizeof(display.description));
+//			file.read((char*)(&display.quantity), sizeof(display.quantity));
+//			file.read((char*)(&display.whole_cost), sizeof(display.whole_cost));
+//			file.read((char*)(&display.retail_cost), sizeof(display.retail_cost));
+//			file.read((&display.date[0]), sizeof(display.date));
+//			buffer.push_back(display);
+//		}
+//
+//		//if (sizeof(buffer[0]) != 0) {
+//		//	cout << sizeof(buffer[0]);
+//		//	cout << endl << buffer.size();
+//		//	num_item = sizeof(buffer) / sizeof(buffer[0]);
+//		//}
+//		//else {
+//		//	num_item = 0;
+//		//}
+//		cout << "There are " << buffer.size() << " items in the inventory\n";
+//		if (buffer.size() > 0) {
+//			cout << "Enter the index of the item you wanna check (first item is 0): \n";
+//			cin >> item_index;
+//		}
+//		if (buffer.size() >= 0 && item_index < buffer.size()) {
+//			cout << "--------------------------------------\n";
+//			cout << "Item description: ";
+//			for (char val : buffer[item_index].description) {
+//				cout << val;
+//			}
+//			cout << endl;
+//			cout << "Quantity on hand: " << buffer[item_index].quantity << endl;
+//			cout << "Wholesale Cost: $" << buffer[item_index].whole_cost << endl;
+//			cout << "Retail Cost: $" << buffer[item_index].retail_cost << endl;
+//			cout << "Date added to inventory: ";
+//			for (char val : buffer[item_index].date) {
+//				cout << val;
+//			}
+//		}
+//		else {
+//			cout << "The index exceed the number of item available\n";
+//			return 2;
+//		}
+//		file.close();
+//		break;
+//	//case 3:
+//	//	item_index = 0;
+//
+//	//	while (file.read((char*)(&change), sizeof(change))) {
+//	//		buffer.push_back(change);
+//	//	}
+//	//	/*if (sizeof(buffer[0]) != 0) {
+//	//		num_item = sizeof(buffer) / sizeof(buffer[0]);
+//	//	}
+//	//	else {
+//	//		num_item = 0;
+//	//	}*/
+//	//	cout << "There are " << buffer.size() << " items in the inventory\n";
+//	//	cout << "Displaying\n";
+//	//	cout << left;
+//	//	cout << "Item Description" << setw(10) << "Quantity" << setw(15) << "Wholesale Cost" << setw(15) << "Retail Cost" << setw(30) << "Date added to inventory\n";
+//	//	for (Item val : buffer) {
+//	//		cout << "Item #" << &val - &buffer[0] << ": \n";
+//	//		cout << val.description << setw(10) << val.quantity << setw(15) << val.whole_cost << setw(15) << val.retail_cost << setw(30) << val.date << endl;
+//	//	}
+//	//	cout << right;
+//	//	cout << "---------------------------\n";
+//	//	cout << "Enter the index of the record you wanna change\n";
+//	//	cin >> item_index;
+//	//	if (item_index >= 0 && item_index < buffer.size()) {
+//	//		cout << "Item index #" << item_index << "chosen\n";
+//	//		cout << "Enter new item description: ";
+//	//		getline(cin, buffer[item_index].description);
+//	//		cout << "Enter new Quantity: ";
+//	//		cin >> buffer[item_index].quantity;
+//	//		cout << "Enter new Wholesale cost: $";
+//	//		cin >> buffer[item_index].whole_cost;
+//	//		cout << "Enter new Retail Cost: $";
+//	//		cin >> buffer[item_index].retail_cost;
+//	//		cout << "Enter new Date added to inventory: ";
+//	//		cin.ignore(100, '\n'); cin.clear(); cin.sync();
+//	//		getline(cin, buffer[item_index].date);
+//	//		file.write((char*)(&buffer), sizeof(buffer));
+//	//	}
+//	//	else {
+//	//		cout << "The index entered for option 3 is wrong\n";
+//	//		return 3;
+//	//	}
+//	//	break;
+//	default:
+//		cout << "Wrong option dummy\n";
+//		break;
+//	}
+//
+//	return 0;
+//}
