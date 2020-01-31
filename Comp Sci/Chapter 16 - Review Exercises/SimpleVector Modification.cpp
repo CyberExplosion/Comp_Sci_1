e3b6b3477e7a2b425afd86d68f0fe7ccd40d3318
CyberExplosion <minhkhoi632000@gmail.com>
2020-01-30 17:48:53 -0800

    Finish up chapter 16

--- Comp Sci/Chapter 16 - Review Exercises/SimpleVector Modification.cpp
+++ Comp Sci/Chapter 16 - Review Exercises/SimpleVector Modification.cpp
@@ -1,69 +1,110 @@
-//#include <iostream>
-//#include <cstdlib>
-//#include <memory>
-//using namespace std;
-//
-////Exception for index out of range
-//struct IndexOutOfRangeException{
-//	const int index;
-//	IndexOutOfRangeException(int ix) : index(ix) {}
-// };
-//
-//template <class T>
-//class SimpleVector {
-//	unique _ptr<T[]> aptr;
-//	int arraySize;
-//public:
-//	SimpleVector(int);
-//	SimpleVector(const SimpleVector&);
-//	int size() const { return arraySize; }
-//	T& operator[](int); //II Overloaded[] operator
-//	void print() const; //II Outputs the array elements
-//
-///*******************************************************
-//Constructor for SimpleVector class. Sets the size
-//of the array and allocates memory for it.
-//*******************************************************/
-//	template < class T>
-//	SimpleVector<T>::SimpleVector(int s) {
-//		arraySize = s;
-//		aptr = make_unique<T[]>(s);
-//		for (int count = O; count < arraySize; count++)
-//			aptr[count] = T();
-//	}
-//	/******************************************************
-//	Copy Constructor for SimpleVector class
-//	******************************************************/
-//	template <class T>
-//	SimpleVector<T>::SimpleVector(const SimpleVector& obj) {
-//		arraySize = obj.arraySize;
-//		aptr = make_unique<T[]>(arraySize);
-//		for (int count = O; count < arraySize; count++)
-//			aptr[count] = obj[count];
-//	}
-//
-//	/*******************************************************
-//	Overloaded[] operator. The argument is a subscript. *
-//	This function returns a reference to the element *
-//	in the array indexed by the subscript . *
-//	/*******************************************************/
-//	template < class T>
-//	T& SimpleVector<T>:: operator [](int sub) {
-//		if (sub < 0 || sub >= arraySize)
-//			throw IndexOutOfRangeException(sub);
-//		return aptr[sub];
-//	}
-//	/********************************************************
-//	prints all the entries i s the array . *
-//	/********************************************************/
-//	template <class T>
-//	void SimpleVector<T> ::print() const {
-//		for (int k = O; k < arraySize; k++)
-//			cout << aptr[k] << " ";
-//		cout << endl;
-//	}
-//};
-//
-//int main() {
-//	return 0;
-//}
+#include <iostream>
+#include <cstdlib>
+#include <memory>
+using namespace std;
+
+//Exception for index out of range
+struct IndexOutOfRangeException{
+	const int index;
+	IndexOutOfRangeException(int ix) : index(ix) {}
+ };
+
+//Exception for full array
+struct ArrayFull {
+	string msg;
+	ArrayFull(string s) : msg(s) {};
+};
+
+//Exception for empty array
+struct Empty {
+	string msg;
+	Empty(string m) : msg(m) {};
+	string what() {
+		return msg;
+	}
+};
+
+template <class T>
+class SimpleVector {
+	unique _ptr<T[]> aptr;
+	int curSize;
+	int arraySize;
+public:
+	SimpleVector(int);
+	SimpleVector(const SimpleVector&);
+	void push_back(T val);
+	void pop_back();
+	int maxSize() const { return arraySize; }
+	int size() const { return curSize; }
+	T& operator[](int); //II Overloaded[] operator
+	void print() const; //II Outputs the array elements
+
+/*******************************************************
+Constructor for SimpleVector class. Sets the size
+of the array and allocates memory for it.
+*******************************************************/
+	template < class T>
+	SimpleVector<T>::SimpleVector(int s) {
+		arraySize = s;
+		//New
+		curSize = 0;
+		aptr = make_unique<T[]>(s);
+		for (int count = O; count < arraySize; count++)
+			aptr[count] = T();
+	}
+	/******************************************************
+	Copy Constructor for SimpleVector class
+	******************************************************/
+	template <class T>
+	SimpleVector<T>::SimpleVector(const SimpleVector& obj) {
+		arraySize = obj.arraySize;
+		cusSize = obj.curSize;
+		aptr = make_unique<T[]>(arraySize);
+		for (int count = O; count < arraySize; count++)
+			aptr[count] = obj[count];
+	}
+
+	template<class T>
+	void SimpleVector<T>::push_back(T val) {
+		if (curSize >= arraySize)
+			throw ArrayFull("Array is full");
+		aptr[curSize] = val;
+		curSize++;
+	}
+
+	template<class T>
+	void SimpleVector<T>::pop_back() {
+		if (curSize == 0)
+			throw Empty("Array empty");
+		curSize--;
+	}
+
+	/*******************************************************
+	Overloaded[] operator. The argument is a subscript. *
+	This function returns a reference to the element *
+	in the array indexed by the subscript . *
+	/*******************************************************/
+	template < class T>
+	T& SimpleVector<T>:: operator [](int sub) {
+		if (sub < 0 || sub >= arraySize)
+			throw IndexOutOfRangeException(sub);
+		return aptr[sub];
+	}
+	/********************************************************
+	prints all the entries i s the array . *
+	/********************************************************/
+	template <class T>
+	void SimpleVector<T> ::print() const {
+		for (int k = O; k < arraySize; k++)
+			cout << aptr[k] << " ";
+		cout << endl;
+	}
+};
+
+int main() {
+	SimpleVector<int>vec(10);
+	for (int i = 0; i < vec.maxSize(); i++) {
+		vec[i] = i;
+	}
+	vec.print();
+}
--- Comp Sci/Tying It All Together/Let's move it - Chapter 15.cpp
+++ Comp Sci/Tying It All Together/Let's move it - Chapter 15.cpp
@@ -1,250 +1,250 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <memory>
-#include <Windows.h>
-using namespace std;
-
-const HANDLE outHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-
-//A shape has a direction and is able to move in that direction.
-//The move is a virtual member function
-
-class Shape {
-private:
-	int dCol, dRow;	//Direction of motion
-public:
-	void setDirection(int drow, int dcol) {
-		dRow = drow;
-		dCol = dcol;
-	}
-	void getDirection(int& drow, int& dcol) const {
-		drow = dRow;
-		dcol = dCol;
-	}
-	virtual void move() = 0;
-};
-
-//A SimpleShape is drawn at a given position in a specified color
-class SimpleShape : public Shape {
-private:
-	int color;
-	int rowPos, colPos;
-public:
-	virtual void draw() const = 0;
-	void getPosition(int& row, int& col) const {
-		row = rowPos;
-		col = colPos;
-	}
-	void setPosition(int row, int col) {
-		rowPos = row;
-		colPos = col;
-	}
-	void setColor(int c) {
-		color = c;
-	}
-	int getColor() const {
-		return color;
-	}
-	virtual void move() override;
-};
-
-//A Box is a rectangular type of shape
-class Box : public SimpleShape {
-private:
-	int width, height;
-public:
-	virtual void draw() const override;
-	Box(int rowPos, int colPos, int width, int height);
-};
-
-//A Tent is an isosceles triangle whose horizontal base has a given length and whose height is half the length of the base. The position of the triangle is the left endpoint of the base
-class Tent : public SimpleShape {
-private:
-	int length;
-public:
-	virtual void draw() const override;
-	Tent(int baseRowPos, int baseColPos, int length);
-};
-
-//A ComplexShape is made up of simpler shapes. It is represented as a vector of pointers to the simpler shapes that make it up
-class ComplexShape : public Shape {
-private:
-	vector<shared_ptr<Shape>>shapes;
-public:
-	ComplexShape(const vector<shared_ptr<Shape>>& shapeCollection);
-	virtual void move() override;
-};
-
-
-/////////////////////////////////////////////////////////////////////
-
-//Now is content of cpp
-
-/************************************
-Moves a simple shape one step by erasing the shape as its current position, changing its position, and then redrawing the shape at its new position.
-************************************/
-void SimpleShape::move() {
-	int dRow, dCol;	//Direction of motion
-	int savedColor = color;
-	color = 0;	//Drawing in color 0 means erases the shape
-	draw();
-
-	//Compute the new position for the shape by adding a step in the proper direction to the current position
-	getPosition(dRow, dCol);
-	rowPos += dRow;
-	colPos += dCol;
-	
-	//Draw the shape at its new position in its specified color
-	color = savedColor;
-	draw();
-}
-
-/******************************
-Draws a tent at its position
-*******************************/
-void Tent::draw() const {
-	int rowPos, colPos;
-	COORD pos;
-	int currentLength = length;
-
-	//Set the color attribute
-	SetConsoleTextAttribute(outHandle, getColor());
-	getPosition(rowPos, colPos);
-	pos.Y = rowPos;
-	pos.X = colPos;
-
-	//Draw the lines that form the tent beginning with the base and moving up toward the point
-	for (int r = 0; r < (length + 1) / 2; r++) {
-		SetConsoleCursorPosition(outHandle, pos);
-		for (int k = 0; k < currentLength; k++) {
-			cout << "*";
-		}
-		cout << endl;
-		pos.Y--;
-		pos.X++;
-		currentLength -= 2;
-	}
-	//Restore normal attribute
-	SetConsoleTextAttribute(outHandle, 7);
-}
-
-/*****************************
-Draws a box shape
-*****************************/
-void Box::draw() const {
-	int rowPos, colPos;
-	COORD pos;
-
-	//Set the color attribute for the box
-	SetConsoleTextAttribute(outHandle, getColor());
-	getPosition(rowPos, colPos);
-	pos.X = colPos;
-	pos.Y = rowPos;
-
-	//Draws the lines that make up the box
-	for (int r = 0; r < height; r++) {
-		SetConsoleCursorPosition(outHandle, pos);
-		for (int c = 0; c < width; c++) {
-			cout << "*";
-		}
-		cout << endl;
-		pos.Y++;
-	}
-	//Restore normal text attribute
-	SetConsoleTextAttribute(outHandle, 7);
-}
-
-/*****************************
-Constructor sets the color, position and dimensions for a box shape, and  draws the box at its initial position
-******************************/
-Box::Box(int rowPos, int colPos, int width, int height) {
-	setColor(4);
-	setPosition(rowPos, colPos);
-	this->width = width;
-	this->height = height;
-	draw();
-}
-
-/*******************************
-Constructor sets the color for a Tent shape, sets the position of the tent as well at the length of its base and draws it at its initial position
-*******************************/
-Tent::Tent(int baseRowPos, int baseColPos, int length) {
-	setColor(2);
-	setPosition(baseRowPos, baseColPos);
-	this->length = length;
-	draw();
-}
-
-/*******************************
-Constructor builds complex shape by assembling a vector of constituent shapes
-********************************/
-ComplexShape::ComplexShape(const vector<shared_ptr<Shape>>& shapeCollection) {
-	for (int k = 0; k < shapeCollection.size(); k++) {
-		auto p = shapeCollection[k];
-		shapes.push_back(p);
-	}
-}
-
-/*****************************
-Moves a complex shape by moving the constituent shapes
-********************************/
-void ComplexShape::move() {
-	for (int k = 0; k < shapes.size(); k++) {
-		shapes[k]->move();
-	}
-}
-
-
-///////////////////////////////////////////////////////
-
-//	MAGIC BEGIN
-
-int main() {
-	//Create a tent and a box
-	shared_ptr<Tent> tent = make_shared<Tent>(20, 10, 13);
-	shared_ptr<Box>box = make_shared<Box>(5, 10, 4, 7);
-
-	//Draw the tent and the box
-	tent->draw();
-	box->draw();
-
-	//Set direction fo motion for the two shapes
-	tent->setDirection(-1, 0);	//Tent moves straight up
-	box->setDirection(0, 1);	//Box moves to the right
-
-	//Simultaneously move the tent and the box
-	for (int k = 0; k < 12; k++) {
-		Sleep(500);
-		tent->move();
-		box->move();
-	}
-	box->move(); 
-	tent->move();
-
-	//Move the box farther to the right
-	for (int k = 0; k < 48; k++) {
-		Sleep(500);
-		box->move();
-	}
-
-
-	//Create a complex shape composed of the tent and the box
-	vector<shared_ptr<Shape>> myShape{ tent, box };
-	shared_ptr<ComplexShape> cS = make_shared<ComplexShape>(myShape);
-
-	//Set direction for the two shapes
-	tent->setDirection(1, 0);
-	box->setDirection(0, -1);
-	//Move the complex shape: this move both the tent and the box
-	for (int k = 0; k < 12; k++) {
-		Sleep(500);
-		cS->move();
-	}
-
-	//Continue moving the box by itself
-	for (int k = 0; k < 36; k++) {
-		Sleep(500);
-		box->move();
-	}
-}
+//#include <iostream>
+//#include <string>
+//#include <vector>
+//#include <memory>
+//#include <Windows.h>
+//using namespace std;
+//
+//const HANDLE outHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+//
+////A shape has a direction and is able to move in that direction.
+////The move is a virtual member function
+//
+//class Shape {
+//private:
+//	int dCol, dRow;	//Direction of motion
+//public:
+//	void setDirection(int drow, int dcol) {
+//		dRow = drow;
+//		dCol = dcol;
+//	}
+//	void getDirection(int& drow, int& dcol) const {
+//		drow = dRow;
+//		dcol = dCol;
+//	}
+//	virtual void move() = 0;
+//};
+//
+////A SimpleShape is drawn at a given position in a specified color
+//class SimpleShape : public Shape {
+//private:
+//	int color;
+//	int rowPos, colPos;
+//public:
+//	virtual void draw() const = 0;
+//	void getPosition(int& row, int& col) const {
+//		row = rowPos;
+//		col = colPos;
+//	}
+//	void setPosition(int row, int col) {
+//		rowPos = row;
+//		colPos = col;
+//	}
+//	void setColor(int c) {
+//		color = c;
+//	}
+//	int getColor() const {
+//		return color;
+//	}
+//	virtual void move() override;
+//};
+//
+////A Box is a rectangular type of shape
+//class Box : public SimpleShape {
+//private:
+//	int width, height;
+//public:
+//	virtual void draw() const override;
+//	Box(int rowPos, int colPos, int width, int height);
+//};
+//
+////A Tent is an isosceles triangle whose horizontal base has a given length and whose height is half the length of the base. The position of the triangle is the left endpoint of the base
+//class Tent : public SimpleShape {
+//private:
+//	int length;
+//public:
+//	virtual void draw() const override;
+//	Tent(int baseRowPos, int baseColPos, int length);
+//};
+//
+////A ComplexShape is made up of simpler shapes. It is represented as a vector of pointers to the simpler shapes that make it up
+//class ComplexShape : public Shape {
+//private:
+//	vector<shared_ptr<Shape>>shapes;
+//public:
+//	ComplexShape(const vector<shared_ptr<Shape>>& shapeCollection);
+//	virtual void move() override;
+//};
+//
+//
+///////////////////////////////////////////////////////////////////////
+//
+////Now is content of cpp
+//
+///************************************
+//Moves a simple shape one step by erasing the shape as its current position, changing its position, and then redrawing the shape at its new position.
+//************************************/
+//void SimpleShape::move() {
+//	int dRow, dCol;	//Direction of motion
+//	int savedColor = color;
+//	color = 0;	//Drawing in color 0 means erases the shape
+//	draw();
+//
+//	//Compute the new position for the shape by adding a step in the proper direction to the current position
+//	getPosition(dRow, dCol);
+//	rowPos += dRow;
+//	colPos += dCol;
+//	
+//	//Draw the shape at its new position in its specified color
+//	color = savedColor;
+//	draw();
+//}
+//
+///******************************
+//Draws a tent at its position
+//*******************************/
+//void Tent::draw() const {
+//	int rowPos, colPos;
+//	COORD pos;
+//	int currentLength = length;
+//
+//	//Set the color attribute
+//	SetConsoleTextAttribute(outHandle, getColor());
+//	getPosition(rowPos, colPos);
+//	pos.Y = rowPos;
+//	pos.X = colPos;
+//
+//	//Draw the lines that form the tent beginning with the base and moving up toward the point
+//	for (int r = 0; r < (length + 1) / 2; r++) {
+//		SetConsoleCursorPosition(outHandle, pos);
+//		for (int k = 0; k < currentLength; k++) {
+//			cout << "*";
+//		}
+//		cout << endl;
+//		pos.Y--;
+//		pos.X++;
+//		currentLength -= 2;
+//	}
+//	//Restore normal attribute
+//	SetConsoleTextAttribute(outHandle, 7);
+//}
+//
+///*****************************
+//Draws a box shape
+//*****************************/
+//void Box::draw() const {
+//	int rowPos, colPos;
+//	COORD pos;
+//
+//	//Set the color attribute for the box
+//	SetConsoleTextAttribute(outHandle, getColor());
+//	getPosition(rowPos, colPos);
+//	pos.X = colPos;
+//	pos.Y = rowPos;
+//
+//	//Draws the lines that make up the box
+//	for (int r = 0; r < height; r++) {
+//		SetConsoleCursorPosition(outHandle, pos);
+//		for (int c = 0; c < width; c++) {
+//			cout << "*";
+//		}
+//		cout << endl;
+//		pos.Y++;
+//	}
+//	//Restore normal text attribute
+//	SetConsoleTextAttribute(outHandle, 7);
+//}
+//
+///*****************************
+//Constructor sets the color, position and dimensions for a box shape, and  draws the box at its initial position
+//******************************/
+//Box::Box(int rowPos, int colPos, int width, int height) {
+//	setColor(4);
+//	setPosition(rowPos, colPos);
+//	this->width = width;
+//	this->height = height;
+//	draw();
+//}
+//
+///*******************************
+//Constructor sets the color for a Tent shape, sets the position of the tent as well at the length of its base and draws it at its initial position
+//*******************************/
+//Tent::Tent(int baseRowPos, int baseColPos, int length) {
+//	setColor(2);
+//	setPosition(baseRowPos, baseColPos);
+//	this->length = length;
+//	draw();
+//}
+//
+///*******************************
+//Constructor builds complex shape by assembling a vector of constituent shapes
+//********************************/
+//ComplexShape::ComplexShape(const vector<shared_ptr<Shape>>& shapeCollection) {
+//	for (int k = 0; k < shapeCollection.size(); k++) {
+//		auto p = shapeCollection[k];
+//		shapes.push_back(p);
+//	}
+//}
+//
+///*****************************
+//Moves a complex shape by moving the constituent shapes
+//********************************/
+//void ComplexShape::move() {
+//	for (int k = 0; k < shapes.size(); k++) {
+//		shapes[k]->move();
+//	}
+//}
+//
+//
+/////////////////////////////////////////////////////////
+//
+////	MAGIC BEGIN
+//
+//int main() {
+//	//Create a tent and a box
+//	shared_ptr<Tent> tent = make_shared<Tent>(20, 10, 13);
+//	shared_ptr<Box>box = make_shared<Box>(5, 10, 4, 7);
+//
+//	//Draw the tent and the box
+//	tent->draw();
+//	box->draw();
+//
+//	//Set direction fo motion for the two shapes
+//	tent->setDirection(-1, 0);	//Tent moves straight up
+//	box->setDirection(0, 1);	//Box moves to the right
+//
+//	//Simultaneously move the tent and the box
+//	for (int k = 0; k < 12; k++) {
+//		Sleep(500);
+//		tent->move();
+//		box->move();
+//	}
+//	box->move(); 
+//	tent->move();
+//
+//	//Move the box farther to the right
+//	for (int k = 0; k < 48; k++) {
+//		Sleep(500);
+//		box->move();
+//	}
+//
+//
+//	//Create a complex shape composed of the tent and the box
+//	vector<shared_ptr<Shape>> myShape{ tent, box };
+//	shared_ptr<ComplexShape> cS = make_shared<ComplexShape>(myShape);
+//
+//	//Set direction for the two shapes
+//	tent->setDirection(1, 0);
+//	box->setDirection(0, -1);
+//	//Move the complex shape: this move both the tent and the box
+//	for (int k = 0; k < 12; k++) {
+//		Sleep(500);
+//		cS->move();
+//	}
+//
+//	//Continue moving the box by itself
+//	for (int k = 0; k < 36; k++) {
+//		Sleep(500);
+//		box->move();
+//	}
+//}
